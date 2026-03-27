d6ffd631f3ff90c39a1c5e3eec9d02816a6b6237
Hemanthkaruturi <karuturihemanth@gmail.com>
2026-03-27 02:40:31 +0000

    fix github issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/kitchenpulse/',
+  base: '/KitchenPulse/',
   plugins: [react()],
   server: {
     port: 3000,
